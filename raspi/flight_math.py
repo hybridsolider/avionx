8f3984c7c5b1257af80db09fe72728d1f9326616
Dominik Szczepański <dominik2010codename@gmail.com>
2025-08-22 21:33:59 +0200

    added speed calculations

--- raspi/flight_math.py
+++ raspi/flight_math.py
@@ -29,4 +29,31 @@ class Course_calc:
         theta = math.atan2(a, b)
         return round(math.degrees(theta))
 
+class Speed_calc:
+    def TAS(altitude, IAS):
+        return IAS + (2 * altitude / 1000)
+    def GS(TAS, wind_speed, wind_angle, heading):
+        wind_angle = math.radians(wind_angle)
+        return math.sqrt(TAS**2 + wind_speed**2 - 2 * TAS * wind_speed * math.cos(wind_angle))
+    
+    
 
+def calc_distance(lat1, lon1, lat2, lon2) -> float:
+    # earth radius
+    # R = 6371.0  km
+    R = 3440.0 # NM
+    
+    #deg -> rad
+    phi1 = math.radians(lat1)
+    phi2 = math.radians(lat2)
+    
+    #calculating Δφ Δλ
+    dphi = math.radians(lat2 - lat1)
+    dlambda = math.radians(lon2 - lon1)
+    
+    # calculating distance
+    a = math.sin(dphi/2)**2 + math.cos(phi1) * math.cos(phi2) * math.sin(dlambda/2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    
+    return distance
